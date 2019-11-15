1716f8eda5f8ab2cda69bdfda8c6a7ae909b1715
cclain594 <claincyril0@gmail.com>
2019-11-15 12:42:56 +0100

    [gitflow]squashing 'feature-creationClassLapin' into 'development'

---
+++ src/main/java/vivants/Lapin.java
@@ -0,0 +1,15 @@
+package vivants;
+
+public class Lapin extends Mammifere{
+
+    @Override
+    public String toString() {
+        return super.toString()+"je suis un lapin";
+
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
