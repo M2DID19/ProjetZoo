3052d30b09f53d01529d7f3547601a17da1a99d2
mastari ayoub <mastariayoub@gmail.com>
2019-11-26 08:52:43 -0800

    suppression de la branche

--- src/main/java/fr/univtln/M2DID19/ProjetZoo/vivants/Mammifere.java
+++ src/main/java/fr/univtln/M2DID19/ProjetZoo/vivants/Mammifere.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class Mammifere extends Animal{
     @Column(name="marin")
     boolean marin;
--- src/main/java/fr/univtln/M2DID19/ProjetZoo/vivants/Oiseau.java
+++ src/main/java/fr/univtln/M2DID19/ProjetZoo/vivants/Oiseau.java
@@ -4,12 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
-
+@MappedSuperclass
 public abstract class Oiseau extends Animal implements Carnivore{
 
 
