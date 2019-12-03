193bc166cd083618b54b1bc9f49b00686a1e1a72
Marcnocito <marc.nocito@hotmail.fr>
2019-12-03 12:10:15 +0100

    merge

--- src/main/java/fr/univtln/M2DID19/ProjetZoo/REST/AigleREST.java
+++ src/main/java/fr/univtln/M2DID19/ProjetZoo/REST/AigleREST.java
@@ -2,8 +2,10 @@ package fr.univtln.M2DID19.ProjetZoo.REST;
 
 import fr.univtln.M2DID19.ProjetZoo.DAO.DAO;
 import fr.univtln.M2DID19.ProjetZoo.ejb.GestionAigle;
+import fr.univtln.M2DID19.ProjetZoo.ejb.GestionAigleLocal;
 import fr.univtln.M2DID19.ProjetZoo.vivants.Aigle;
 
+import javax.ejb.EJB;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,27 +14,44 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/aigle")
-//@Produces({"application/json})"})
+@Produces({"application/json", "application/xml"})
 public class AigleREST {
 
 //    @Inject DAO dao;
 //    @Inject GestionAigle gestionAigle;
     GestionAigle gestionAigle = new GestionAigle();
-//    @GET
-//    @Path("/aigles")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<Aigle> getAllAigle() {
-//        return dao.findWithNamedQuery("findAllAigle");
-//    }
+    Aigle aigle = new Aigle();
+//    @EJB GestionAigleLocal gestionAigle;
+
+
+    @GET
+    @Path("/allAigles")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Aigle> getAllAigle() {
+        return gestionAigle.findAllAigle();
+    }
+
 
     @GET
     @Path("/aigles")
-//    @Produces(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getAllAigle() {
-//        gestionAigle.findAllAigle();
-        gestionAigle.communiquer();
-        return "got it";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Aigle getAigle() {
+        aigle = gestionAigle.findAigle();
+        System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD \n\n\n" + aigle + "\n\n\n\n");
+        return aigle;
     }
+
+
+//    @GET
+//    @Path("/aigles")
+////    @Produces(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getAllAigle() {
+////        gestionAigle.findAllAigle();
+//        gestionAigle.communiquer();
+//        System.out.println("HALLO");
+////        gestionAigle.findAllAigle();
+//        return "got it";
+//    }
 
 }
