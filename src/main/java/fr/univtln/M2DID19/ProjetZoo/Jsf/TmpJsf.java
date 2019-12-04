d6f3b44a1254e5aa461fb85027560c1027891352
Marcnocito <marc.nocito@hotmail.fr>
2019-12-04 15:15:26 +0100

    affiche la page d'accueil avec jsf

--- src/main/java/fr/univtln/M2DID19/ProjetZoo/Jsf/TmpJsf.java
+++ src/main/java/fr/univtln/M2DID19/ProjetZoo/Jsf/TmpJsf.java
@@ -2,32 +2,19 @@ package fr.univtln.M2DID19.ProjetZoo.Jsf;
 //import javax.faces.context.ExternalContext;
 //import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import java.io.Serializable;
 //import java.io.IOException;
 
-@Named
-public class TmpJsf {
-    private String nom;
-    private String mdp;
+@Named("TmpJsf")
+public class TmpJsf implements Serializable {
+    private String message = "bonjour";
 
-    public String getMdp() {
-        return mdp;
+
+    public String getMessage() {
+        return message;
     }
 
-    public String getNom() {
-        return nom;
+    public void setMessage(String string) {
+        message = string;
     }
-
-    public void setMdp(String string) {
-        mdp = string;
-    }
-
-    public void setNom(String string) {
-        nom = string;
-    }
-
-//    public void logout() throws IOException {
-//        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-//        ec.invalidateSession();
-//        ec.redirect(ec.getRequestContextPath() + "/index.html");
-//    }
 }
