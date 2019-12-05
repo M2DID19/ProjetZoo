3334f35dd791b71de6e777f96516bb92d7ec2187
Marcnocito <marc.nocito@hotmail.fr>
2019-12-06 00:25:27 +0100

    redirect OK

--- src/main/java/fr/univtln/M2DID19/ProjetZoo/Jsf/TmpJsf.java
+++ src/main/java/fr/univtln/M2DID19/ProjetZoo/Jsf/TmpJsf.java
@@ -1,11 +1,12 @@
 package fr.univtln.M2DID19.ProjetZoo.Jsf;
-//import javax.faces.context.ExternalContext;
-//import javax.faces.context.FacesContext;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
-//import java.io.IOException;
 
 @Named("TmpJsf")
+@SessionScoped
+@ManagedBean
 public class TmpJsf implements Serializable {
     private String message = "bonjour";
 
