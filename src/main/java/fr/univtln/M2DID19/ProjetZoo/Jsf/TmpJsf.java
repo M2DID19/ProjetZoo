f486e5720af4c2b022f68e1daa430994da8309a3
Marcnocito <marc.nocito@hotmail.fr>
2019-12-12 17:56:27 +0100

    modification jsfPrimeFaces

--- src/main/java/fr/univtln/M2DID19/ProjetZoo/Jsf/TmpJsf.java
+++ src/main/java/fr/univtln/M2DID19/ProjetZoo/Jsf/TmpJsf.java
@@ -1,17 +1,34 @@
 package fr.univtln.M2DID19.ProjetZoo.Jsf;
-import javax.annotation.ManagedBean;
-import javax.enterprise.context.SessionScoped;
+import fr.univtln.M2DID19.ProjetZoo.ejb.GestionAigle;
+import fr.univtln.M2DID19.ProjetZoo.vivants.Aigle;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @Named("TmpJsf")
-@SessionScoped
-@ManagedBean
+@RequestScoped
 public class TmpJsf implements Serializable {
     private String message = "bonjour";
+    @EJB GestionAigle gestionAigle;
 
+    private List<Aigle> liste;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("????????????????????");
+        liste = gestionAigle.findAllAigle();
+    }
+
+    public List<Aigle> getListe() {
+        return liste;
+    }
 
     public String getMessage() {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
         return message;
     }
 
